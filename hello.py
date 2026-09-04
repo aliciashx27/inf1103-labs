abf6bef3e24d04bdee4758d2f78589750210ac66
Alicia Seah <aliciashx@gmail.com>
2026-09-04 14:49:27 +0800

    User Profile

--- hello.py
+++ hello.py
@@ -1,22 +1,9 @@
-print("============================")
-print("Welcome here")
-print("My first post!")
-print("============================")
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
-
-followers += 50  # Increase followers by 50
-print("Day 1:", followers)
-
-followers += 20  # Increase followers by 20
-print("Day 2:", followers)
-
-followers -= 10  # Decrease followers by 10
-print("Day 3:", followers)
-
-
+print("\nInstagram Profile")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Age:", age)
+print("Category:", category)
+
