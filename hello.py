44adb80abf16e4ce9281f85aad9a5a6f143443d0
Alicia Seah <aliciashx@gmail.com>
2026-09-04 15:13:30 +0800

    Added Activity 1, 2 and 3 to hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,31 @@
+#Activity 1
+print("===============================")
+print("Welcome here")
+print("My first post!")
+print("===============================")
+
+#Activity 2
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#Activity 3
+followers = 100
+
+followers += 50
+print("Day 1", followers)
+
+followers += 20
+print("Day 2", followers)
+
+followers -= 30
+print("Day 3", followers)
+
+#Activity 4 + 5
 username = input("Enter Username: ")
 age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
